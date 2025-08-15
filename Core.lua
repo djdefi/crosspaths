950da0ab462258b4012871cc648b8df7c664dcd2
GitHub Action <action@github.com>
2025-08-15 04:16:27 +0000

    Bump version from 0.1.16 to 0.1.17

--- Core.lua
+++ Core.lua
@@ -5,7 +5,7 @@ local addonName, Crosspaths = ...
 
 -- Create the main addon object
 Crosspaths = Crosspaths or {}
-Crosspaths.version = "0.1.16"
+Crosspaths.version = "0.1.17"
 Crosspaths.debug = false
 
 -- Default settings
